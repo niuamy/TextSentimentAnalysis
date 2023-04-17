3eb4ec2ef92f93ed2b59b5c835595ade0fb9c8d8
Amy Niu <amy@amys-mbp.attlocal.net>
2023-04-17 15:54:47 -0400

    remove unnecessary comments

--- convert.py
+++ convert.py
@@ -3,13 +3,6 @@ import requests
 
 apiKey = 'YOUR API KEY'
 
-# Using https://api-ninjas.com/, the objective of this project is to
-# take a given sentence and convert it into a less a negative version.
-# Given a piece of text, it uses sentiment analysis to determine a negativity score of a given word.
-# It then uses a thesaurus api to find a synonym of this word and uses sentiment analysis to determine the least negative synonym of this word.
-# Finally, it prints out the original and new text.
-# The goal is not to change the meaning of the text itself but simply to convert the text into a less negative version.
-
 def getSynonym(word):
     api_url = 'https://api.api-ninjas.com/v1/thesaurus?word={}'.format(word)
     response = requests.get(api_url, headers={'X-Api-Key': apiKey})
